5d8ed450693ab8932eea8d431987ed257e8a2dc6
Alejandro Valencia Blancas <alezombi@gmail.com>
2022-10-13 17:39:40 +0200

    PV2 avanzando
    Co-Authored-By: vizan161 <83875556+vizan161@users.noreply.github.com>

--- Práctica Voluntaria/PV2/js.js
+++ Práctica Voluntaria/PV2/js.js
@@ -5,6 +5,14 @@
 
 "use strict";
 
+
+/*class tarea {
+    text;
+    done;
+    tags;
+};*/
+
+/*
 let planetas = [
     "Mercurio", "Venus", "Tierra",
     "Marte", "Júpiter", "Saturno",
@@ -13,4 +21,33 @@ let planetas = [
 
 planetas.forEach(p => {
     console.log(`¡Hola, ${p}!`);
-});
+});*/
+
+let cosas = [
+    "universidad", "basico"
+]
+let listaTareas = [
+    { text: "Preparar practicas AW", tags: ["universidad", "awt"] }
+    , { text: "Mirar fechas congreso", done: true, tags: [] }
+    , { text: "Ir al supermercado", tags: ["personal", "basico"] }
+    , { text: "Jugar al futbol", done: false, tags: ["personal", "deportes"] }
+    , { text: "Hablar con profesor", done: false, tags: ["universidad", "tp2"] }
+];
+function getToDoTasks(listaTareas) {
+    return listaTareas.filter(n => n.done != true).map(n => n["text"]);
+};
+
+console.log(getToDoTasks(listaTareas));
+
+function findByTag(listaTareas, tag) {
+    return listaTareas.filter(n => n.tags.indexOf(tag) != -1);
+};
+
+console.log(findByTag(listaTareas, "awt"));
+
+function findByTags(listaTareas, tags) {
+    // return listaTareas.filter(n => n.tags.some();
+    return listaTareas.filter(n => tags.some(p => n.tags.includes(p)));
+}
+
+console.log(findByTags(listaTareas, cosas));
