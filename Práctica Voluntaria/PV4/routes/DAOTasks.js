95dc7c4a06fd1c0bdbb41fc6805494e3c066795e
vizan161 <83875556+vizan161@users.noreply.github.com>
2022-11-24 19:46:58 +0100

    daotask

--- Práctica Voluntaria/PV4/routes/DAOTasks.js
+++ Práctica Voluntaria/PV4/routes/DAOTasks.js
@@ -29,24 +29,24 @@ class DAOTasks {
                 console.log(`error: conexion con base de datos fallida: ${error.message}`);
                 callback(error);
             } else {
-                const sql = "SELECT * FROM aw_tareas_tareas JOIN tag ON taskId = id WHERE user = ? ";
+                const sql = "SELECT * FROM tareas JOIN tareas_etiquetas ON idTarea = idTarea JOIN user_tareas ON idTarea = idTarea JOIN usuarios ON idUser = idUser WHERE email = ? ";
                 connection.query(sql, [email], function (error, aux) {
                     connection.release();
                     if (error) {
                         callback(error);
                     } else {
                         const array = [];
-                        const tags = [];
+                        const etiquetas = [];
 
                         aux.forEach(element => {
-                            tags.push(element.tag);
+                            etiquetas.push(element.etiqueta);
                         });
 
                         array.push({
                             "id": aux[0].id,
                             "text": aux[0].text,
                             "done": aux[0].done,
-                            "tags": tags,
+                            "etiquetas": etiquetas,
                         });
 
                         callback(null, array);
@@ -56,7 +56,7 @@ class DAOTasks {
         });
     }
 
-    insertTask(email, task, callback) {
+    insertTask(email, tarea, callback) {
         this.pool.getConnection(function (error, connection) {
 
             if (error) {
@@ -64,18 +64,27 @@ class DAOTasks {
                 console.log(`error: conexion con base de datos fallida: ${error.message}`);
                 callback(error);
             } else {
-                const sql = "INSERT INTO task(user, text, done) VALUES(?, ?, ?)";
-                connection.query(sql, [email, task.text, task.done], function (error, resultado) {
+                const sql = "INSERT INTO tareas(text) VALUES(?)";
+                connection.query(sql, [tarea.text], function (error, resultado) {
                     connection.release();
                     if (error) {
                         callback(error);
                     } else {
                         const values = [];
-                        task.tags.forEach(e => {
+                        tarea.etiquetas.forEach(e => {
                             values.push(resultado.insertId, e);
                         });
-                        const sql1 = `INSERT INTO tag (taskId, tag) VALUES ${generateSQLColumns(values, task.tags)}`;
+                        const sql1 = `INSERT INTO etiquetas (idEtiqueta, texto) VALUES ${generateSQLColumns(values, tarea.etiquetas)}`;
                         connection.query(sql1, values, function (error) {
+                            connection.release();
+                            if (error) {
+                                callback(error);
+                            } else {
+                                callback();
+                            }
+                        });
+                        const sql2 = `INSERT INTO tareas_etiquetas (idEtiqueta, idTarea) VALUES (?, ?)`;
+                        connection.query(sql2, [tarea.etiqueta.idEtiqueta, tarea.idTarea], function (error) {
                             connection.release();
                             if (error) {
                                 callback(error);
@@ -89,14 +98,14 @@ class DAOTasks {
         });
     }
 
-    markTaskDone(idTask, callback) {
+    markTaskDone(idTarea, callback) {
         this.pool.getConnection(function (error, connection) {
             if (error) {
                 console.log(`error: conexion con base de datos fallida: ${error.message}`);
                 callback(error);
             } else {
-                const sql = "UPDATE task SET done = 1 WHERE id = ?";
-                connection.query(sql, [idTask], function (error) {
+                const sql = "UPDATE tarea SET done = 1 WHERE id = ?";
+                connection.query(sql, [idTarea], function (error) {
                     connection.release();
                     if (error) {
                         callback(error);
@@ -114,7 +123,7 @@ class DAOTasks {
                 console.log(`error: conexion con base de datos fallida: ${error.message}`);
                 callback(error);
             } else {
-                const sql = "DELETE FROM task WHERE user = ? AND done = 1;";
+                const sql = "DELETE FROM tareas JOIN user_tareas ON idTarea = idTarea JOIN usuarios ON idUser = idUser WHERE email = ? AND done = 1;";
                 connection.query(sql, [email], function (error) {
                     connection.release();
                     if (error) {
