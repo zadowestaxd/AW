1be4f7df85b9cd9e2dbb581c5120a17cfa38b8aa
Alejandro Valencia Blancas <alezombi@gmail.com>
2023-06-10 20:45:18 +0200

    ejercicio 2 acabado

--- Examen/IntentoExamen/Examem/Ejercicio 2/app.js
+++ Examen/IntentoExamen/Examem/Ejercicio 2/app.js
@@ -12,6 +12,8 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
+const matriz = [[0, 0, 0, 0], [0, 0, 0, 0][0, 0, 0, 0]];
+
 //Gestiona el enlace cuando entras en http://localhost:3000
 app.get("/", function (request, response) {
 
@@ -25,18 +27,46 @@ app.get("/login", function (request, response) {
 
 });
 
+app.get("/resultados", function (request, response) {
+    //matriz[0][0] = 0;
+    response.render("resultados", { matriz: matriz });
+
+});
+
+app.get("/gracias", function (request, response) {
+
+    response.render("gracias");
+
+});
+
+
 //Coge los datos pasados por el formulario del login
 app.post("/login", function (request, response) {
     console.log(request.body);
-    if (request.body.edad == "<20") {
-
+    if (request.body.edad == "menor que 20") {
+        matriz[0][0]++;
     } else if (request.body.edad == "20 a 30") {
-
-    } else if (request.body.edad == ">40") {
-
+        matriz[0][1]++;
+    } else if (request.body.edad == "30 a 40") {
+        matriz[0][2]++;
+    } else if (request.body.edad == "mayor de 40") {
+        matriz[0][3]++;
     }
 
+    if (request.body.canal == "Buscador de internet") {
+        matriz[1][1]++;
+    }
+    else if (request.body.edad == "Comunidad de Madrid") {
+        matriz[1][1]++;
+    } else if (request.body.edad == "resto del territorio") {
+        matriz[1][2]++;
+    } else if (request.body.edad == "fuera de España") {
+        matriz[1][3]++;
+    }
+    response.redirect("/gracias");
 });
+
+
 
 //conecta con el servidor
 app.listen(3000, function (error) {
